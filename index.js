5bbf997503b56b5cf628a860b95ebcd0002209aa
administrator <mkantor@vmware.com>
2016-07-06 06:34:11 -0700

    Updates

--- index.js
+++ index.js
@@ -8,26 +8,35 @@ var tessel = require('tessel');
 var servolib = require('servo-pca9685');
 var servo = servolib.use(tessel.port['A']);
 var servo1 = 1; // We have a servo plugged in at position 1
-var servoStopValue = 0.385;
-
-app.use(bodyParser);
+var servoStopValue = 0.383;
 
 servo.on('ready', function () {
     console.log("SERVO READY");
-    var position = 0;  //  Target position of the servo between 0 (min) and 1 (max).
 
 
 	// respond with "hello world" when a GET request is made to the homepage
     app.get('/', function(req, res) {
-	var value = Number(req.query.move_value);
-	console.log("RECEIVED GET: " + value);
-	if (!isNaN(value)) {
-	    try {
-		servo.move(servo1, value);
-	    } catch(e) {
-		console.error(e);
+	if (req.query.move_speed) {
+	    var value = Number(req.query.move_speed);
+	    console.log("RECEIVED GET: " + value);
+	    if (!isNaN(value)) {		
+		try {
+		    servo.move(servo1, value);
+		} catch(e) {
+		    console.error(e);
+		}
 	    }
+	} else if (req.query.move_count) {
+	    var count = Math.max(4, Number(req.query.move_count));
+	    console.log("Message received; count: " + count);
+	    for (var i = 0; i < count; i++ ) {
+		setTimeout(function() {
+		    ding();
+		}, 500 * i);
+	    }
+	    res.status(200).send('OK')
 	}
+
 	console.log("OK");
 	res.status(200).send('OK2')
     });
@@ -44,22 +53,9 @@ servo.on('ready', function () {
 	    console.error(e);
 	}
     }
-
-    app.post('/', function(req, res) {
-	console.log("RECEIVED POST");
-	    var matches = req.body.text.match(/:bell:/g) || [];
-	    var count = Math.min(4, matches.length);
-	    console.log("Message received; count: " + count);
-	    for (var i = 0; i < count; i++ ) {
-		setTimeout(function() {
-		    ding();
-		}, 500 * i);
-	    }
-	    res.status(200).send('OK')
-	});
 });
 
-var fs = require('fs');
+/*var fs = require('fs');
 try {
   var secureServer = https.createServer({
     key: key,
@@ -71,9 +67,9 @@ try {
   console.error('FAILED: ', e);
 }
 if (secureServer) app = secureServer;
+*/
 
+// Listen on port 9019, IP defaults to 192.168.1.101. Also accessible through [tessel-name].local
+app.listen(9019);
 
-// Listen on port 8080, IP defaults to 192.168.1.101. Also accessible through [tessel-name].local
-app.listen(8080);
-
-console.log("Server running at http://192.168.1.101:8080/");
+console.log("Server running at http://xxx.xxxx.xxx.xxx:9019/ - use t2 wifi");
